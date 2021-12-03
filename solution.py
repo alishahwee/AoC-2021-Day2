2f8165d3ec472216c5f2661641196300ff586442
alishahwee <alisha.hwee@outlook.com>
2021-12-02 21:46:40 -0600

    Solve part two

--- solution.py
+++ solution.py
@@ -16,4 +16,19 @@ def part_one(commands: list[str]) -> int:
 
 
 def part_two(commands: list[str]) -> int:
-    pass
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+
+    for command in commands:
+        direction, units = command.split()
+        units = int(units)
+        if direction == "forward":
+            horizontal_position += units
+            depth += units * aim
+        elif direction == "down":
+            aim += units
+        elif direction == "up":
+            aim -= units
+
+    return horizontal_position * depth
